cdc6ef8015ec2fea13eefb1999f3e135bee530f4
ErlingHW <erlingstaff@gmail.com>
2022-03-07 16:34:01 +0100

    dataset class created

---
+++ MLP - Boston Housing/boston-dataset.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from sklearn.datasets import load_boston
+from sklearn.preprocessing import StandardScaler
+
+class BostonDataset(torch.utils.data.Dataset):
+  def __init__(self, X, y, scale_data=True):
+    if not torch.is_tensor(X) and not torch.is_tensor(y):
+      if scale_data:
+          X = StandardScaler().fit_transform(X)
+      self.X = torch.from_numpy(X)
+      self.y = torch.from_numpy(y)
+
+  def __len__(self):
+      return len(self.X)
+
+  def __getitem__(self, i):
+      return self.X[i], self.y[i]
