da84527ef2595618fa9ca77423d55455e92cd9ee
ErlingHW <erlingstaff@gmail.com>
2022-03-08 10:22:47 +0100

    mlp cleanup

--- MLP/mlp.py
+++ MLP/mlp.py
@@ -28,56 +28,29 @@ class MLP(nn.Module):
   
 if __name__ == '__main__':
   
-  # Set fixed random number seed
   torch.manual_seed(42)
-  
-  # Prepare CIFAR-10 dataset
   dataset = CIFAR10(os.getcwd(), download=True, transform=transforms.ToTensor())
   trainloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True, num_workers=1)
-  
-  # Initialize the MLP
   mlp = MLP()
-  
-  # Define the loss function and optimizer
   loss_function = nn.CrossEntropyLoss()
   optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
   
-  # Run the training loop
-  for epoch in range(0, 5): # 5 epochs at maximum
-    
-    # Print epoch
+  for epoch in range(0, 5):
     print(f'Starting epoch {epoch+1}')
-    
-    # Set current loss value
     current_loss = 0.0
     
-    # Iterate over the DataLoader for training data
     for i, data in enumerate(trainloader, 0):
-      
-      # Get inputs
       inputs, targets = data
-      
-      # Zero the gradients
       optimizer.zero_grad()
-      
-      # Perform forward pass
       outputs = mlp(inputs)
-      
-      # Compute loss
       loss = loss_function(outputs, targets)
-      
-      # Perform backward pass
       loss.backward()
-      
-      # Perform optimization
       optimizer.step()
       
-      # Print statistics
       current_loss += loss.item()
       if i % 500 == 499:
           print('Loss after mini-batch %5d: %.3f' %
                 (i + 1, current_loss / 500))
           current_loss = 0.0
 
-  # Process is complete.
   print('Training process has finished.')
