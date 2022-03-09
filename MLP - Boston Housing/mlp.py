2624ad06f3ecc92d2f2a0443e1f8857842ed6baf
ErlingHW <erlingstaff@gmail.com>
2022-03-09 15:29:10 +0100

    spaces -> tab indentation

--- MLP - Boston Housing/mlp.py
+++ MLP - Boston Housing/mlp.py
@@ -5,9 +5,6 @@ from sklearn.datasets import load_boston
 from sklearn.preprocessing import StandardScaler
 
 class BostonDataset(torch.utils.data.Dataset):
-  '''
-  Prepare the Boston dataset for regression
-  '''
 
   def __init__(self, X, y, scale_data=True):
     if not torch.is_tensor(X) and not torch.is_tensor(y):
@@ -25,84 +22,79 @@ class BostonDataset(torch.utils.data.Dataset):
       
 
 class MLP(nn.Module):
-  '''
-    Multilayer Perceptron for regression.
-  '''
-  def __init__(self):
-    super().__init__()
-    self.layers = nn.Sequential(
-      nn.Linear(13, 64),
-      nn.ReLU(),
-      nn.Linear(64, 32),
-      nn.ReLU(),
-      nn.Linear(32, 1)
-    )
+	#Multilayer Perceptron for regression.
+	
+	def __init__(self):
+		super().__init__()
+		self.layers = nn.Sequential(
+    	nn.Linear(13, 64),
+    	nn.ReLU(),
+    	nn.Linear(64, 32),
+    	nn.ReLU(),
+    	nn.Linear(32, 1)
+   		)
 
 
-  def forward(self, x):
-    '''
-      Forward pass
-    '''
-    return self.layers(x)
+	def forward(self, x):
+		#Forward pass
+		return self.layers(x)
 
   
 if __name__ == '__main__':
   
-  # Set fixed random number seed
-  torch.manual_seed(42)
+	# Set fixed random number seed
+	torch.manual_seed(42)
   
-  # Load Boston dataset
-  X, y = load_boston(return_X_y=True)
+	# Load Boston dataset
+	X, y = load_boston(return_X_y=True)
   
-  # Prepare Boston dataset
-  dataset = BostonDataset(X, y)
-  trainloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True, num_workers=1)
+	# Prepare Boston dataset
+	dataset = BostonDataset(X, y)
+	trainloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True, num_workers=1)
   
-  # Initialize the MLP
-  mlp = MLP()
+	# Initialize the MLP
+	mlp = MLP()
   
-  # Define the loss function and optimizer
-  loss_function = nn.L1Loss()
-  optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
+	# Define the loss function and optimizer
+	loss_function = nn.L1Loss()
+	optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
   
-  # Run the training loop
-  for epoch in range(0, 100): # 5 epochs at maximum
+	# Run the training loop
+	for epoch in range(0, 100): # 5 epochs at maximum
+    	# Print epoch
+		print(f'Starting epoch {epoch+1}')
     
-    # Print epoch
-    print(f'Starting epoch {epoch+1}')
+	# Set current loss value
+	current_loss = 0.0
     
-    # Set current loss value
-    current_loss = 0.0
-    
-    # Iterate over the DataLoader for training data
-    for i, data in enumerate(trainloader, 0):
+	# Iterate over the DataLoader for training data
+	for i, data in enumerate(trainloader, 0):
+		# Get and prepare inputs
+		inputs, targets = data
+		inputs, targets = inputs.float(), targets.float()
+		targets = targets.reshape((targets.shape[0], 1))
       
-      # Get and prepare inputs
-      inputs, targets = data
-      inputs, targets = inputs.float(), targets.float()
-      targets = targets.reshape((targets.shape[0], 1))
+		# Zero the gradients
+		optimizer.zero_grad()
       
-      # Zero the gradients
-      optimizer.zero_grad()
+		# Perform forward pass
+		outputs = mlp(inputs)
       
-      # Perform forward pass
-      outputs = mlp(inputs)
+		# Compute loss
+		loss = loss_function(outputs, targets)
       
-      # Compute loss
-      loss = loss_function(outputs, targets)
+		# Perform backward pass
+		loss.backward()
       
-      # Perform backward pass
-      loss.backward()
+		# Perform optimization
+		optimizer.step()
       
-      # Perform optimization
-      optimizer.step()
-      
-      # Print statistics
-      current_loss += loss.item()
-      if i % 10 == 0:
-          print('Loss after mini-batch %5d: %.3f' %
-                (i + 1, current_loss / 500))
-          current_loss = 0.0
+		# Print statistics
+		current_loss += loss.item()
+		if i % 10 == 0:
+			print('Loss after mini-batch %5d: %.3f' %
+				(i + 1, current_loss / 500))
+			current_loss = 0.0
 
-  # Process is complete.
-  print('Training process has finished.')
+# Process is complete.
+print('Training process has finished.')
